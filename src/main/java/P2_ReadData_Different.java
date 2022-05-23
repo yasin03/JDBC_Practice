a0ba604650ae925c64865a66176284dbde2a49e1
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-23 15:43:36 +0300

    23.05.22

---
+++ src/main/java/P2_ReadData_Different.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class P2_ReadData_Different {
+
+        //MySql de day5 de..
+
+        // ilk satirdaki ilk objeyi yazdiralim
+
+        // ikinci satirdaki ilk objeyi yazdiralim
+
+        // ilk sutundaki 5.degeri yazdiralim
+
+
+        // 0.satira gitmemizin sebebi while sorgusu icerisinde bulunan resultSet.next() in bizi
+        // birinci kayda goturmesi icindir. Eger 1.satira gidecek olsak while icindeki kod yuzunden yazdirmaya
+        // 2.satirdan baslar, tum listeyi alamayiz
+
+        // Tum listeyi yazdirmak istersek;
+
+
+}
+
