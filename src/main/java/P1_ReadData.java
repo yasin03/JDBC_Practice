08db4f4b22d3daa014f44f06f8d0e35456c8a600
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-23 15:41:53 +0300

    23.05.22

---
+++ src/main/java/P1_ReadData.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class P1_ReadData {
+
+      //MySql de day3 de..
+
+
+    // SORU1: Talebeler tablosundan tum kayıtları listeleyiniz.
+
+    // SORU2: Talebeler tablosunda notları 90 uzeri olan kayitları listeleyiniz.
+
+    // SORU3: Talebeler tablosunda id'si 124 olan öğrencilerin tüm bilgilerini listeleyiniz.
+
+    // SORU4: Talebeler tablosunda notu 70 ile 90 arasindaki kisilerin isimlerini listeleyiniz.
+
+    // SORU5: Talebeler tablosunda isminin 2. harfi e olan kisilerin veli_isimleri ile birlikte listeleyiniz.
+
+
+
+    }
+
+
